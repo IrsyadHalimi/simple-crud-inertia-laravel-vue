4c3ac5803ebefce169f36f06f092dad7df0ba88e
IrsyadHalimi <irsyadhalimi08@gmail.com>
2025-01-09 14:10:00 +0700

    add tailwind class

--- resources/js/Pages/Book/Edit.vue
+++ resources/js/Pages/Book/Edit.vue
@@ -1,22 +1,39 @@
 <template>
-    <div>
-        <h1>Edit Book</h1>
+    <div class="container w-80 mx-auto mt-8">
+        <h1 class="text-2xl font-bold mb-4">Edit Book</h1>
         <form :action="`/books/${book.id}`" method="POST">
             <input type="hidden" name="_method" value="PUT" />
             <input type="hidden" name="_token" :value="csrf" />
-            <div>
-                <label>Title</label>
-                <input type="text" name="title" :value="book.title" />
+            <div class="py-3">
+                <label for="title" class="block font-medium text-gray-900">Title</label>
+                <div class="flex items-center rounded-md bg-white pl-3 outline outline-1 -outline-offset-1 outline-gray-300 has-[input:focus-within]:outline has-[input:focus-within]:outline-2 has-[input:focus-within]:-outline-offset-2 has-[input:focus-within]:outline-indigo-600 shadow-sm">
+                    <input type="text" name="title" id="title" class="block min-w-0 grow py-1.5 pl-1 pr-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline focus:outline-0 sm:" placeholder="Insert book title.." :value="book.title" />
+                </div>
             </div>
-            <div>
-                <label>Author</label>
-                <input type="text" name="author" :value="book.author" />
+            <div class="py-3">
+                <label for="author" class="block font-medium text-gray-900">Author</label>
+                <div class="flex items-center rounded-md bg-white pl-3 outline outline-1 -outline-offset-1 outline-gray-300 has-[input:focus-within]:outline has-[input:focus-within]:outline-2 has-[input:focus-within]:-outline-offset-2 has-[input:focus-within]:outline-indigo-600 shadow-sm">
+                    <input type="text" name="author" id="author" class="block min-w-0 grow py-1.5 pl-1 pr-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline focus:outline-0 sm:" placeholder="Insert book author.." :value="book.author" />
+                </div>
             </div>
-            <div>
-                <label>Year</label>
-                <input type="number" name="year" :value="book.year" />
+            <div class="py-3">
+                <label for="year" class="block font-medium text-gray-900">Year</label>
+                <div class="flex items-center rounded-md bg-white pl-3 outline outline-1 -outline-offset-1 outline-gray-300 has-[input:focus-within]:outline has-[input:focus-within]:outline-2 has-[input:focus-within]:-outline-offset-2 has-[input:focus-within]:outline-indigo-600 shadow-sm">
+                    <input type="text" name="year" id="year" class="block min-w-0 grow py-1.5 pl-1 pr-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline focus:outline-0 sm:" placeholder="Insert book year.." :value="book.year" />
+                </div>
             </div>
-            <button type="submit">Save</button>
+            <a href="/books">
+                <button type="button" class="bg-blue-400 text-white my-8 py-2 px-4 rounded-md hover:bg-blue-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+                        <path fill-rule="evenodd" d="M9.53 2.47a.75.75 0 0 1 0 1.06L4.81 8.25H15a6.75 6.75 0 0 1 0 13.5h-3a.75.75 0 0 1 0-1.5h3a5.25 5.25 0 1 0 0-10.5H4.81l4.72 4.72a.75.75 0 1 1-1.06 1.06l-6-6a.75.75 0 0 1 0-1.06l6-6a.75.75 0 0 1 1.06 0Z" clip-rule="evenodd" />
+                    </svg>
+                </button>
+            </a>
+            <button type="submit" class="bg-indigo-600 text-white my-8 py-2 px-4 rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 ml-2">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 12.75 6 6 9-13.5" />
+                </svg>
+            </button>
         </form>
     </div>
 </template>
--- resources/js/Pages/Book/Index.vue
+++ resources/js/Pages/Book/Index.vue
@@ -1,27 +1,44 @@
 <template>
-    <div>
-        <h1>Book List</h1>
-        <a href="/books/create">Add new book</a>
-        <table>
-            <thead>
+    <div class="container mx-auto mt-8">
+        <h1 class="text-2xl font-bold mb-4">Book List</h1>
+        <a href="/books/create" title="add new book" class="inline-block bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 mb-4">
+            <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6.03v13m0-13c-2.819-.831-4.715-1.076-8.029-1.023A.99.99 0 0 0 3 6v11c0 .563.466 1.014 1.03 1.007 3.122-.043 5.018.212 7.97 1.023m0-13c2.819-.831 4.715-1.076 8.029-1.023A.99.99 0 0 1 21 6v11c0 .563-.466 1.014-1.03 1.007-3.122-.043-5.018.212-7.97 1.023"/>
+            </svg>
+        </a>
+        <table class="min-w-full bg-white border border-gray-200 shadow-md rounded">
+            <thead class="bg-gray-100">
                 <tr>
-                    <th>Title</th>
-                    <th>Author</th>
-                    <th>Year</th>
-                    <th>Action</th>
+                    <th class="px-4 py-2 border-b text-left text-gray-600 font-medium">Title</th>
+                    <th class="px-4 py-2 border-b text-left text-gray-600 font-medium">Author</th>
+                    <th class="px-4 py-2 border-b text-left text-gray-600 font-medium">Year</th>
+                    <th class="px-4 py-2 border-b text-left text-gray-600 font-medium">Action</th>
                 </tr>
             </thead>
             <tbody>
-                <tr v-for="book in books" :key="book.id">
-                    <td>{{ book.title }}</td>
-                    <td>{{ book.author }}</td>
-                    <td>{{ book.year }}</td>
-                    <td>
-                        <a :href="`/books/${book.id}/edit`">Edit</a>
-                        <form :action="`/books/${book.id}`" method="POST" style="display: inline;">
+                <tr v-for="book in books" :key="book.id" class="hover:bg-gray-50">
+                    <td class="px-4 py-2 border-b">{{ book.title }}</td>
+                    <td class="px-4 py-2 border-b">{{ book.author }}</td>
+                    <td class="px-4 py-2 border-b">{{ book.year }}</td>
+                    <td class="px-4 py-2 border-b flex space-x-2">
+                        <a :href="`/books/${book.id}/edit`"
+                            class="text-blue-500 hover:underline" title="Edit book">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+                                <path d="M21.731 2.269a2.625 2.625 0 0 0-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 0 0 0-3.712ZM19.513 8.199l-3.712-3.712-12.15 12.15a5.25 5.25 0 0 0-1.32 2.214l-.8 2.685a.75.75 0 0 0 .933.933l2.685-.8a5.25 5.25 0 0 0 2.214-1.32L19.513 8.2Z" />
+                            </svg>
+                        </a>
+                        <form :action="`/books/${book.id}`" method="POST" class="inline">
                             <input type="hidden" name="_method" value="DELETE" />
                             <input type="hidden" name="_token" :value="csrf" />
-                            <button type="submit">Delete</button>
+                            <button
+                                type="submit"
+                                class="text-red-500 hover:underline"
+                                onclick="return confirm('Are you sure you want to delete this book?');" title="Delete book">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+                                    <path d="M3.375 3C2.339 3 1.5 3.84 1.5 4.875v.75c0 1.036.84 1.875 1.875 1.875h17.25c1.035 0 1.875-.84 1.875-1.875v-.75C22.5 3.839 21.66 3 20.625 3H3.375Z" />
+                                    <path fill-rule="evenodd" d="m3.087 9 .54 9.176A3 3 0 0 0 6.62 21h10.757a3 3 0 0 0 2.995-2.824L20.913 9H3.087Zm6.133 2.845a.75.75 0 0 1 1.06 0l1.72 1.72 1.72-1.72a.75.75 0 1 1 1.06 1.06l-1.72 1.72 1.72 1.72a.75.75 0 1 1-1.06 1.06L12 15.685l-1.72 1.72a.75.75 0 1 1-1.06-1.06l1.72-1.72-1.72-1.72a.75.75 0 0 1 0-1.06Z" clip-rule="evenodd" />
+                                </svg>
+                            </button>
                         </form>
                     </td>
                 </tr>
